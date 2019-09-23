1e84a5f1374534869d56c754a24f7f0d59883d9a
haolong <haolong@51jianjiao.com>
2019-09-23 23:00:23 +0800

    Revert "Merge branch 'test'"
    This reverts commit e2e3c60d6e9a0cbbc9f16622965ce90b4dae0eac, reversing
    changes made to b491c2d1fcf817b318a8ef8cb9e9cee1a101fbd0.

--- buy.py
+++ buy.py
@@ -41,13 +41,6 @@ print tasks_number
 while i < tasks_number:
     t = threading.Thread(target=test(i))
     t.start()
-    t.start()
-    t.start()
-    t.start()
-    i +=1
-    i +=1
-    i +=1
-    i +=1
     i +=1
 
 
